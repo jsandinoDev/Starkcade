1be5e54a24ace3ed4bfeabbda9b793752a8de860
RareMmemshima <Ihuwediane@gmail.com>
2025-01-29 14:38:12 +0100

    mod: Created a modal that explains the game rules
    Signed-off-by: RareMmemshima <Ihuwediane@gmail.com>

--- packages/nextjs/app/coinflip/_components/CoinflipPage.tsx
+++ packages/nextjs/app/coinflip/_components/CoinflipPage.tsx
@@ -7,31 +7,10 @@ import { ConflipPlayPage } from './ConflipPlayPage';
 import { QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
 import GenericModal from '~~/components/scaffold-stark/CustomConnectButton/GenericModal';
 import { tips } from '~~/utils/Constants';
+import Modal from '~~/components/ui/Modal';
 
 
-export const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
 
-  return (
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-      onClick={onClose}
-    >
-      <div
-        className="modal-box modal-border bg-modal  rounded-[8px] border flex flex-col gap-3 justify-around relative "
-        onClick={(e) => e.stopPropagation()}
-      >
-        <button
-          onClick={onClose}
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
-        >
-          ✕
-        </button>
-        {children}
-      </div>
-    </div>
-  );
-};
 
 export const CoinflipPage = () => {
 
