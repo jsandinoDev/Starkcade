17fbfad2899cfc7838020ba080bae7103cf10630
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-13 21:16:45 +0000

    test 12

--- packages/nextjs/app/recentflips/_components/RecentFlip.tsx
+++ packages/nextjs/app/recentflips/_components/RecentFlip.tsx
@@ -1,9 +1,32 @@
 "use client";
 import Image from "next/image";
-import { MyFlips } from "~~/app/assets/constants";
+
 
 
 export default function RecentFlip() {
+
+ const MyFlips = [
+    {
+      imageSrc: "/happy_coin.png",
+      description: "You flipped 0.01 and doubled",
+      time: "5",
+      imageAlt: "Happy Coin",
+    },
+    {
+      imageSrc: "/sad_coin.png",
+      description: "You flipped 2.5 and got rugged",
+      time: "7",
+      imageAlt: "Sad Coin",
+    },
+    {
+      imageSrc: "/happy_coin.png",
+      description: "You flipped 6 and doubled",
+      time: "10",
+      imageAlt: "Happy Coin",
+    },
+  ];
+
+
   const handleButtonClick = () => {
     console.log("GO BACK button clicked");
   };
@@ -14,7 +37,7 @@ export default function RecentFlip() {
       <div className="recentflip w-[40%] max-w-[560px] min-w-[280px] h-auto">
         <h2 className="text-2xl mb-10 w-full text-gray-800">MY RECENT FLIPS</h2>
         <div className="space-y-1">
-          {/* {MyFlips.map((flip, index) => (
+          {MyFlips.map((flip, index) => (
             <div
               key={index}
               className={`flex items-center justify-between pb-1  ${index < 2 ? "border-b border-gray-400 " : ""}`}
@@ -39,9 +62,7 @@ export default function RecentFlip() {
               </div>
               <p className="text-[#333] text-xs">{flip.time} minutes ago</p>
             </div>
-          ))} */}
-
-          test 11
+          ))}
         </div>
         <div className="mt-5 mb-5 ">
           <button
